9622446690436aab0a660fb5f7e5f69e2d85497c
deepakjadiwal <deepak.jadiwal@switchmyloan.in>
2025-10-29 19:15:21 +0530

    remove card from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,9 +39,9 @@ const Home = () => {
     <div className="p-6 flex-1 overflow-auto">
       <h1 className="text-4xl font-bold text-gray-800 mb-6">Dashboard</h1>
 
-      {/* Grid container for future multiple cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {/* Total Leads Card */}
+     
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      
         <div
           onClick={() => navigate("/leads")}
           className="cursor-pointer flex items-center justify-between p-6 border rounded-2xl shadow-sm hover:shadow-md transition bg-blue-50 border-blue-200"
@@ -56,7 +56,7 @@ const Home = () => {
             <Users className="h-8 w-8 text-blue-500" />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
